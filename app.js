fa2dbf098fa58eca970f582309b96c1bb72e38ff
Tonnywin <13638430047@163.com>
2017-08-15 18:06:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
-var list = [
+
+//存取localStorage中的数据
+
+var store = {
+	save(key,value){
+		localStorage.setItem(key,JSON.stringify(value));
+	},
+	fetch(key){
+		return JSON.parse(localStorage.getItem(key)) || [];
+	}
+}
+
+/*var list = [
 	{
 		title:"吃饭打豆豆",
-		isChecked:false //状态为false，为不选中  任务未完成
-	},
-	{
-		title:"起来敲代码啦~",
-		isChecked:true   //状态为true，为选中    任务完成
+		isChecked:true // 状态为false，为不选中  任务未完成
 	}
-];
+];*/
 
-new Vue({
+//取出所有的值
+var list = store.fetch("miaov-new-class");
+
+//过滤的时候有三种情况 all finished unfinished
+
+var filter = {
+	all:function(list){
+		return list;
+	},
+	finished:function(list){
+		return list.filter(function(item){
+			return item.isChecked;
+		})
+	},
+	unfinished:function(){
+		return list.filter(function(item){
+			return !item.isChecked;
+		})
+	}
+}
+
+var vm = new Vue({
 	el:".main",
 	data:{
 		list:list,
 		todo:"",
 		edtorTodos:'',  //记录正在编辑的数据
-		beforeTitle:'' //记录正在编辑的数据的title
+		beforeTitle:'', //记录正在编辑的数据的title
+		visibility: "all" //通过这个属性值的变化对数据进行筛选
+	},
+	watch:{
+		/*list:function(){  //监控list这个属性，当这个属性对应的值发生变化就会执行函数
+			store.save("miaov-new-class",this.list);
+		}*/
+		list:{
+			handler:function(){
+				store.save("miaov-new-class",this.list);
+			},
+			deep:true
+		}
 	},
 	computed:{
 		noCheckeLength:function(){
 			return this.list.filter(function(item){
                 return !item.isChecked
             }).length
+		},
+		filteredList:function(){			
+			//找到了过滤函数，就返回过滤后的数据；如果没有返回所有数据
+			return filter[this.visibility] ? filter[this.visibility](list) : list;
+
 		}
 	},
 	methods:{
@@ -31,10 +77,12 @@ new Vue({
 				isChecked:false
 			});
 			this.todo = '';
+
 		},
 		deleteTodo(todo){ //删除任务
 			var index = this.list.indexOf(todo);
 			this.list.splice(index,1);
+
 		},
 		edtorTodo(todo){  //编辑任务
 			console.log(todo);
@@ -42,6 +90,7 @@ new Vue({
 			this.beforeTitle = todo.title;
 
 			this.edtorTodos = todo;
+
 
 
 		},
@@ -68,3 +117,14 @@ new Vue({
 		}
 	}
 });
+
+function watchHashChange(){
+	var hash = window.location.hash.slice(1);
+
+	vm.visibility = hash;
+	
+}
+
+watchHashChange();
+
+window.addEventListener("hashchange",watchHashChange);
